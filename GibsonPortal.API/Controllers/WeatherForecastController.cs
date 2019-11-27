63a07b2428f86f557957cf9100d98dbd70107691
Nic <destrada@gibsonplumbing.com>
2019-11-27 09:13:06 -0600

    End of Section 4

--- GibsonPortal.API/Controllers/WeatherForecastController.cs
+++ GibsonPortal.API/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace GibsonPortal.API.Controllers
             _context = context;
 
         }
-
+        [AllowAnonymous]
         [HttpGet]
 
         public async Task<IActionResult> GetValues()
